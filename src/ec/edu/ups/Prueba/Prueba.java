f6aadaaf63cbe268d9c9aa5326a78c1ad26177e3
Carmen <Carmen@LAPTOP-QE836KAF>
2019-04-14 20:31:06 -0500

    Clase anonima y uso de operador instanceOf

--- src/ec/edu/ups/Prueba/Prueba.java
+++ src/ec/edu/ups/Prueba/Prueba.java
@@ -8,7 +8,12 @@ package ec.edu.ups.Prueba;
 import ec.edu.ups.Clases.Auto;
 import ec.edu.ups.Clases.Camion;
 import ec.edu.ups.Clases.Cliente;
+import ec.edu.ups.Clases.ConsesionarioVehiculos;
 import ec.edu.ups.Clases.Funcionario;
+import ec.edu.ups.Clases.Persona;
+import ec.edu.ups.Clases.Vehiculo;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @version 1.2
@@ -16,6 +21,7 @@ import ec.edu.ups.Clases.Funcionario;
  * @author Carmen Bravo
  *
  */
+//clase prueba
 public class Prueba {
 
     /**
@@ -23,6 +29,11 @@ public class Prueba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("***// UNIVERSIDAD POLITECNICA SALESIANA //***");
+        System.out.println("***PROGRAMACION ORIENTADA A OBJETOS***");
+        
+        
+        
         Cliente CarlosP = new Cliente("Tarjeta", "26568948", true, "0107593873", "Carlos", "Pelaez", "3012092", 1, "Av. Las Americas");
         System.out.println("");
         System.out.println(CarlosP);
@@ -58,6 +69,63 @@ public class Prueba {
 
         System.out.println("");
 
+        
+        
+         //arraylist est se debe a que tenmos herencia
+        List<ConsesionarioVehiculos> lista = new ArrayList<>();
+        lista.add(CarlosP);
+        lista.add(AndresM);
+        lista.add(chevrolet);
+        lista.add(mercedesBenz);
+
+        /**
+         * recorrido
+         */
+        for (ConsesionarioVehiculos consesionario : lista) {
+            if (consesionario instanceof Auto) {
+                Auto t1 = (Auto) consesionario;
+                System.out.println(" Auto " + t1);
+            } else if (consesionario instanceof Camion) {
+                Camion t2 = (Camion) consesionario;
+                System.out.println(" Camioneta " + t2);
+            } else if (consesionario instanceof Funcionario) {
+                Funcionario t3 = (Funcionario) consesionario;
+                System.out.println(" funcionario "  + t3);
+            } else if (consesionario instanceof Cliente) {
+                Cliente t4 = (Cliente) consesionario;
+                System.out.println(" cliente "+ t4);
+            }
+        }
+
+        System.out.println("");
+    
+    
+        /**
+         * clase anonima
+         */
+        Vehiculo vehiculoAnonimo = new Vehiculo("Chevrolet", "Rojo", "Familiar", "AGH-859", 3, "Av.Las americas") {
+            @Override
+            public String gustar() {
+                return this.getMarca() + " Esta marca es muy buena";
+            }
+
+        };
+        
+        System.out.println(vehiculoAnonimo);
+        System.out.println(vehiculoAnonimo.gustar());
+       
+
+        Persona personaAnonimo = new Persona("0107593873", "Antony", "Jurado", "3012092", 4, "   Av. Las americas") {
+            @Override
+            public String conducir() {
+                return this.getNombre()+ " esta conduciendo";
+            }
+
+        };
+       
+        System.out.println(personaAnonimo);
+        System.out.println(personaAnonimo.conducir());
+    }
     }
 
-}
+
